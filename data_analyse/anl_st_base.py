0e75d87710f1bf2a2c48578837248de8ea9602ac
16104 <744234398@qq.com>
2018-01-24 17:29:08 +0800

    保存

--- data_analyse/anl_st_base.py
+++ data_analyse/anl_st_base.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import pandas as pd
+import numpy as np
 import json
 
 class AnlStBase(object):
@@ -19,8 +20,35 @@ class AnlStBase(object):
         data = json.loads(r)
         #a_js = json.dumps(data)
         df = pd.DataFrame(data)
-        print(df.head(1))
+        return df
+
+    def axisData(self, *args):
+        dfs = []
+        for i in args:
+            dfs.append(self.importJSON(i))
+        _df = pd.merge(dfs[0], dfs[1])
+        _df = pd.merge(_df, dfs[2])
+        _df = pd.merge(_df, dfs[3])
+        _df = pd.merge(_df, dfs[4])
+
+        _df.to_excel("D:/py/data/dfs_ex.xlsx")
+
+        #增加净利润资产率=净利润/总资产
+        _df['profit_assets'] = _df['net_profits']/_df['totalAssets']
+        _df.fillna(0)  #0填充NaN
+        _df = _df.loc[:,['code','name','industry','nav','roe','gross_profit_rate','profit_assets','currentasset_turnover','rateofreturn']]
+        _df = _df.dropna(axis=0)
+        _df = _df.sort_values(by=['nav','roe','currentasset_turnover','rateofreturn','gross_profit_rate','profit_assets'])
+        return _df
 
 if __name__ == "__main__":
     a = AnlStBase("D:\py\\ts")
-    a.importJSON("get_stock_basics.json")
+    #a.importJSON("get_stock_basics.json")
+    dfs = a.axisData(
+        "get_stock_basics.json",
+        "get_profit_data.json",
+        "get_operation_data.json",
+        "get_growth_data.json",
+        "get_cashflow_data.json"
+    )
+    dfs.to_excel("D:/py/data/dfs.xlsx")
