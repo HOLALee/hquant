db566493abca382aafca372acf038441f8497d07
hola <744234398@qq.com>
2018-02-07 19:06:15 +0800

    dd

--- fs_pe.py
+++ fs_pe.py
@@ -2,21 +2,28 @@
 
 from gmsdk.api import StrategyBase
 
+
 class Mystrategy(StrategyBase):
     def __init__(self, *args, **kwargs):
         super(Mystrategy, self).__init__(*args, **kwargs)
 
 
     def on_login(self):
+        print 'login~~~'
         pass
 
     def on_error(self, code, msg):
+        print code
         pass
 
     def on_tick(self, tick):
+        self.open_long(tick.exchange, tick.sec_id, tick.last_price, 100)
+        print("OpenLong: exchange %s, sec_id %s, price %s" %
+                (tick.exchange, tick.sec_id, tick.last_price))
         pass
 
     def on_bar(self, bar):
+        print 'get bar data,do something~~~'
         pass
 
     def on_execrpt(self, res):
@@ -46,12 +53,12 @@ class Mystrategy(StrategyBase):
 
 if __name__ == '__main__':
     myStrategy = Mystrategy(
-        username='-',
-        password='-',
+        username='18923744450',
+        password='lyz880522',
         strategy_id='4786a29b-09b7-11e8-bce8-76dfbf6cddb5',
-        subscribe_symbols='SZSE.000513.tick,SZSE.000513.bar.15',
+        subscribe_symbols='SZSE.000513.tick',
         mode=4,
-        td_addr='127.0.0.1:8001'
+        td_addr=''
     )
     myStrategy.backtest_config(
         start_time='2000-02-01 09:30:00',
