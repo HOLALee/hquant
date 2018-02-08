994302dc4381ad24a1e23a20c66f0143f6011706
hola <744234398@qq.com>
2018-02-08 22:42:09 +0800

    解决冲突

--- strategys/value_dig.py
+++ strategys/value_dig.py
@@ -1,3 +1,4 @@
+<<<<<<< current
 # -*- coding: UTF-8 -*-
 import datetime
 from rqalpha.api import *
@@ -165,3 +166,90 @@ config = {
 
 if __name__ == '__main__':
     run_func(init=init, before_trading=before_trading, handle_bar=handle_bar, config=config)
+=======
+# -*- coding: UTF-8 -*-
+import datetime
+from rqalpha.api import *
+from rqalpha import run_func
+import tushare as ts
+import pandas as pd
+import numpy as np
+
+#在这个方法中编写任何的初始化逻辑。context对象将会在你的算法策略的任何方法之间做传递。
+def init(context):
+    select_stocks()
+    # 在context中保存全局变量
+    context.s1 = "000001.XSHE"
+    # 实时打印日志
+    logger.info("RunInfo: {}".format(context.run_info))
+
+# before_trading此函数会在每天策略交易开始前被调用，当天只会被调用一次
+def before_trading(context):
+    logger.info("开盘前执行before_trading函数")
+
+# 你选择的证券的数据更新将会触发此段逻辑，例如日或分钟历史数据切片或者是实时数据切片更新
+def handle_bar(context, bar_dict):
+    logger.info("每一个Bar执行")
+    logger.info("打印Bar数据：")
+    #logger.info(bar_dict[context.s1])
+
+# after_trading函数会在每天交易结束后被调用，当天只会被调用一次
+def after_trading(context):
+    logger.info("收盘后执行after_trading函数")
+
+#
+def select_stocks():
+    #估值指标：
+    # pe_ratio  市盈率
+    # pcf_ratio 市现率
+    # pb_ratio 市净率
+    # ps_ratio 市销率
+    #财务指标
+    # adjusted_diluted_earnings_per_share 每股收益EPS - 扣除/期末股本摊薄
+    # book_value_per_share 每股净资产BPS
+    # return_on_equity 净资产收益率
+    # adjusted_return_on_equity_diluted 净资产收益率ROE(扣除/摊薄)
+    # return_on_asset_net_profit 总资产净利率ROA
+    #利润指标
+    # net_profit 净利润
+    # gross_profit主营业务利润
+    data = get_fundamentals(query(
+                fundamentals.eod_derivative_indicator.pe_ratio,
+                fundamentals.eod_derivative_indicator.pcf_ratio,
+                fundamentals.eod_derivative_indicator.pb_ratio,
+                fundamentals.eod_derivative_indicator.ps_ratio,
+                fundamentals.financial_indicator.adjusted_diluted_earnings_per_share,
+                fundamentals.financial_indicator.book_value_per_share,
+                fundamentals.financial_indicator.return_on_equity,
+                fundamentals.financial_indicator.adjusted_return_on_equity_diluted,
+                fundamentals.financial_indicator.return_on_asset_net_profit,
+                fundamentals.income_statement.net_profit,
+                fundamentals.income_statement.gross_profit
+            )
+        )
+
+    return data
+
+#策略配置
+config = {
+  "base": {
+    "start_date": "2016-06-01",
+    "end_date": "2016-12-01",
+    "benchmark": "000300.XSHG",
+    "accounts": {
+        "stock": 100000
+    }
+  },
+  "extra": {
+    "log_level": "verbose",
+  },
+  "mod": {
+    "sys_analyser": {
+      "enabled": True,
+      "plot": True
+    }
+  }
+}
+
+run_func(init=init, before_trading=before_trading, handle_bar=handle_bar, config=config)
+>>>>>>> before discard
